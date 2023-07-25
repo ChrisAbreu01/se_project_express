89cf7f54e258b5d109850ae3d605b6bc8d43d3dd
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-07-25 16:33:34 -0400

    feat! Validating Inbound Server Data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mongoose = require("mongoose");
 const errorHandler = require("./middlewares/error-handler");
 
@@ -12,6 +13,7 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(errors());
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
