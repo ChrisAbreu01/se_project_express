6aa04a4f6752d1bff6ab183f18641f5f0aea5c43
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-08-02 15:00:48 -0400

    fix! changing ports to enable connection

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const errorHandler = require("./middlewares/error-handler");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
