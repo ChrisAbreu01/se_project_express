e1f4db192c5dd425bf3432f6ddfe6a96efd27338
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-08-08 21:33:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,14 @@ const routes = require("./routes");
 
 app.use(express.json());
 app.use(cors());
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(requestLogger);
-app.use(routes);
+app.use("/", routes);
 
 app.use(errorLogger);
 
